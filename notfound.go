e0e5c44850b5a65aa7f0fea51664f8c2dbc3ce06
Trond Kandal <Trond.Kandal@ntnu.no>
2022-11-15 21:09:43 +0100

    Some convenience handlers.

---
+++ notfound.go
@@ -0,0 +1,17 @@
+package handlers
+
+/*
+ * Copyright (c) 2022 Norwegian University of Science and Technology
+ */
+
+import (
+	"go.uber.org/zap"
+	"net/http"
+)
+
+func NotFound(log *zap.SugaredLogger) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Infof("path not found %s", r.URL.Path)
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+	}
+}
